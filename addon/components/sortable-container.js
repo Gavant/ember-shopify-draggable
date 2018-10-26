bc9b717ea8db25cbf15ad4bf33ccad50ceb7f0c9
Adam Baker <adambaker@adams-mbp.gavant.com>
2018-10-26 13:40:53 -0400

    Working on modifying underlying js object order

--- addon/components/sortable-container.js
+++ addon/components/sortable-container.js
@@ -1,16 +1,59 @@
 import Component from '@ember/component';
 import layout from '../templates/components/sortable-container';
-import { get } from '@ember/object';
+import { get, set } from '@ember/object';
+import { tryInvoke } from '@ember/utils';
+import { A } from '@ember/array';
+import { next } from '@ember/runloop';
 
 export default Component.extend({
     layout,
     classNames: ['sortable-container'],
+    items: null,
+    actions: {
+        dragStart(item) {
+            set(this, 'group.dragItem', item);
+        }
+    },
     didInsertElement() {
         this._super(...arguments);
         get(this, 'group.sortable').addContainer(this.element);
+        get(this, 'group.sortable').on('drag:stop',() => {
+            next(this, () => {
+                const element = document.getElementById(get(this, 'dragEventNode.id'));
+                if (element) {
+                    element.parentNode.removeChild(element);
+                }
+                set(this, 'group.dragItem', null);
+                set(this, 'dragEventNode', null);
+            });
+
+        });
+        get(this, 'group.sortable').on('sortable:stop', (event) => {
+            const items = A(get(this, 'items').toArray());
+            const targetContainer = get(event, 'data.newContainer');
+            const targetIndex = get(event, 'data.newIndex');
+            const oldContainer = get(event, 'data.oldContainer');
+            const oldIndex = get(event, 'data.oldIndex');
+            const item = get(this, 'group.dragItem');
+            //Sorted within this container
+            if (this.element.isSameNode(targetContainer) && this.element.isSameNode(oldContainer)) {
+                items.removeAt(oldIndex, 1);
+                items.insertAt(targetIndex, item);
+                tryInvoke(this, 'itemReordered', [items, item, event]);
+            } else if (this.element.isSameNode(targetContainer)) { // added to this container
+                items.insertAt(targetIndex, item);
+                tryInvoke(this, 'itemAdded', [items, item, event]);
+                set(this, 'dragEventNode', get(event, 'data.dragEvent.source'));
+            } else if (this.element.isSameNode(oldContainer)) { // removed from this container
+                items.removeAt(oldIndex, 1);
+                tryInvoke(this, 'itemRemoved', [items, item, event]);
+            }
+        });
     },
     willDestroyElement() {
         this._super(...arguments);
         get(this, 'group.sortable').removeContainer(this.element);
     },
+}).reopenClass({
+    positionalParams: ['items']
 });
--- addon/components/sortable-item.js
+++ addon/components/sortable-item.js
@@ -1,7 +1,22 @@
 import Component from '@ember/component';
 import layout from '../templates/components/sortable-item';
+import { tryInvoke } from '@ember/utils';
+import { get } from '@ember/object';
 
 export default Component.extend({
     layout,
-    classNames: ['sortable-item']
+    classNames: ['sortable-item'],
+    visible: false,
+    didInsertElement() {
+        this._super(...arguments);
+
+        get(this, 'container.group.sortable').on('drag:start', (event) => {
+            const source = get(event, 'data.originalSource');
+            if (this.element && this.element.isSameNode(source)) {
+                tryInvoke(this, 'dragStart', [get(this, 'item')]);
+            }
+        });
+    }
+}).reopenClass({
+    positionalParams: ['item']
 });
