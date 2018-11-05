c2e2e7601e4f728c959bc49c1eabe14db4f69cc1
Adam Baker <adambaker@adams-mbp.gavant.com>
2018-11-05 14:04:23 -0500

    Improving dummy app

---
+++ tests/dummy/app/controllers/swappable.js
@@ -0,0 +1,13 @@
+import Controller from '@ember/controller';
+import { A } from '@ember/array';
+
+export default Controller.extend({
+    list: A([
+        { name: "Item 1" },
+        { name: "Item 2" },
+        { name: "Item 3" },
+        { name: "Item 4" },
+        { name: "Item 5" },
+        { name: "Item 6" }
+    ])
+});
