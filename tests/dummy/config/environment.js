982f2a1fb5492a04deecac2cac6e7402a73a87d6
Adam Baker <adambaker@adams-mbp.gavant.com>
2018-11-05 10:36:18 -0500

    Demo work

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -44,6 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/ember-shopify-draggable/';
     // here you can enable a production-specific feature
   }
 
