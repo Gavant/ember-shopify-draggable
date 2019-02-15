5c95f9822da9d0cad07871fadb06a083e03186a7
Adam Baker <bakerac4@gmail.com>
2019-02-15 14:24:30 -0500

    Changing ember-cli-es6-transform version to be locked at 0.0.5

--- blueprints/ember-shopify-draggable/index.js
+++ blueprints/ember-shopify-draggable/index.js
@@ -2,6 +2,6 @@
 module.exports = {
    normalizeEntityName() {},
    afterInstall() {
-     return this.addPackagesToProject([{ name: 'ember-cli-es6-transform'},{ name: '@shopify/draggable'}]);
+     return this.addPackagesToProject([{ name: 'ember-cli-es6-transform', target: '0.0.5'},{ name: '@shopify/draggable'}]);
    },
 };
