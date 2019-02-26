d46dc9f2b2f643360d2f776d500b2235c28ce22f
Adam Baker <bakerac4@gmail.com>
2019-02-26 08:41:41 -0500

    Updating tests

--- tests/integration/components/swappable-group-test.js
+++ tests/integration/components/swappable-group-test.js
@@ -2,25 +2,62 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { A } from '@ember/array';
 
 module('Integration | Component | swappable-group', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('Renders else block if no items given', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{swappable-group}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
+    this.set('list', A());
     await render(hbs`
-      {{#swappable-group}}
-        template block text
-      {{/swappable-group}}
-    `);
+        {{#swappable-group as |group|}}
+            {{#group.container list
+                itemReordered=(action (mut list))
+                itemAdded=(action (mut list))
+                itemRemoved=(action (mut list))
+                as |container|}}
+                    {{#each container.items as |item index|}}
+                        {{#container.item item index=index}}
+                            {{item.name}}
+                        {{/container.item}}
+                    {{else}}
+                        Im empty
+                    {{/each}}
+            {{/group.container}}
+        {{/swappable-group}}`
+    );
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.textContent.trim(), 'Im empty');
+  });
+
+  test('Renders list item', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    this.set('list', A([
+        { name: "Item 1" }
+    ]));
+    await render(hbs`
+        {{#swappable-group as |group|}}
+            {{#group.container list
+                itemReordered=(action (mut list))
+                itemAdded=(action (mut list))
+                itemRemoved=(action (mut list))
+                as |container|}}
+                    {{#each container.items as |item index|}}
+                        {{#container.item item index=index}}
+                            {{item.name}}
+                        {{/container.item}}
+                    {{else}}
+                        Im empty
+                    {{/each}}
+            {{/group.container}}
+        {{/swappable-group}}`
+    );
+
+    assert.equal(this.element.textContent.trim(), 'Item 1');
   });
 });
