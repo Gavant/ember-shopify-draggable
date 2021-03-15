055668628674221a138293527940bf197fb0419d
Adam Baker <bakerac4@gmail.com>
2021-03-15 09:33:17 -0400

    Work on upgrading to glimmer components

--- index.js
+++ index.js
@@ -4,14 +4,7 @@ module.exports = {
     name: '@gavant/ember-shopify-draggable',
     options: {
         babel: {
-            plugins: [
-                // Ensure that `ember-auto-import` can handle the dynamic imports
-                require('ember-auto-import/babel-plugin')
-            ]
+            plugins: [require.resolve('ember-auto-import/babel-plugin')]
         }
-    },
-
-    included() {
-        this._super.included.apply(this, arguments);
     }
 };
--- tests/dummy/app/controllers/sortable.js
+++ tests/dummy/app/controllers/sortable.js
@@ -1,88 +1,74 @@
 import Controller from '@ember/controller';
-import { get } from '@ember/object';
-import { A } from '@ember/array';
+import { action, get } from '@ember/object';
 
-export default Controller.extend({
-    logEvents: false,
-    resizeMirror: false,
-    snappable: false,
-    swapAnimation: false,
-    collidable: false,
-    collidables: '.container-list > header',
-    list: A([
-        { name: "Item 1" },
-        { name: "Item 2" },
-        { name: "Item 3" }
-    ]),
-    listTwo: A([
-        { name: "Item 4" },
-        { name: "Item 5" },
-        { name: "Item 6" }
-    ]),
-    listThree: A([
-        { name: "Item 7" },
-        { name: "Item 8" },
-        { name: "Item 9" }
-    ]),
+export default class SortableController extends Controller {
+    logEvents = false;
+    resizeMirror = false;
+    snappable = false;
+    swapAnimation = false;
+    collidable = false;
+    collidables = '.container-list > header';
+    list = [{ name: 'Item 1' }, { name: 'Item 2' }, { name: 'Item 3' }];
+    listTwo = [{ name: 'Item 4' }, { name: 'Item 5' }, { name: 'Item 6' }];
+    listThree = [{ name: 'Item 7' }, { name: 'Item 8' }, { name: 'Item 9' }];
 
+    @action
     logEvent(name, event) {
-        if(get(this, 'logEvents')) {
+        if (get(this, 'logEvents')) {
             //eslint-disable-next-line no-console
             console.log(`${name} fired!`, event);
         }
-    },
-
-    actions: {
-        sortableStart(event) {
-            this.logEvent('sortable:start', event);
-        },
-
-        sortableSort(event) {
-            this.logEvent('sortable:sort', event);
-        },
-
-        sortableSorted(event) {
-            this.logEvent('sortable:sorted', event);
-        },
-
-        sortableStop(event) {
-            this.logEvent('sortable:stop', event);
-        },
-
-        dragStart(event) {
-            this.logEvent('drag:start', event);
-        },
-
-        dragMove(event) {
-            this.logEvent('drag:move', event);
-        },
-
-        dragOver(event) {
-            this.logEvent('drag:over', event);
-        },
-
-        dragOut(event) {
-            this.logEvent('drag:out', event);
-        },
-
-        dragStop(event) {
-            this.logEvent('drag:stop', event);
-        },
-
-        snapIn(event) {
-            this.logEvent('snap:in', event);
-        },
-
-        snapOut(event) {
-            this.logEvent('snap:out', event);
-        },
-
-        collidableIn(event) {
-            this.logEvent('collidable:in', event);
-        },
-
-        collidableOut(event) {
-            this.logEvent('collidable:out', event);
-        }
     }
-});
+    @action
+    sortableStart(event) {
+        this.logEvent('sortable:start', event);
+    }
+    @action
+    sortableSort(event) {
+        this.logEvent('sortable:sort', event);
+    }
+    @action
+    sortableSorted(event) {
+        this.logEvent('sortable:sorted', event);
+    }
+    @action
+    sortableStop(event) {
+        this.logEvent('sortable:stop', event);
+    }
+    @action
+    dragStart(event) {
+        this.logEvent('drag:start', event);
+    }
+    @action
+    dragMove(event) {
+        this.logEvent('drag:move', event);
+    }
+    @action
+    dragOver(event) {
+        this.logEvent('drag:over', event);
+    }
+    @action
+    dragOut(event) {
+        this.logEvent('drag:out', event);
+    }
+    @action
+    dragStop(event) {
+        this.logEvent('drag:stop', event);
+    }
+    @action
+    snapIn(event) {
+        this.logEvent('snap:in', event);
+    }
+    @action
+    snapOut(event) {
+        this.logEvent('snap:out', event);
+    }
+    @action
+    collidableIn(event) {
+        this.logEvent('collidable:in', event);
+    }
+    @action
+    collidableOut(event) {
+        this.logEvent('collidable:out', event);
+    }
+}
